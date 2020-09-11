c3eccb6d5c7975db1a0edebcca4583d6b05b220e
yeetornghoo <carlson.hoo@gmail.com>
2020-09-11 12:50:04 +0800

    a

--- dataset/SemEval2018_Task1/1_label_sentiment.py
+++ dataset/SemEval2018_Task1/1_label_sentiment.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from Controller import FileController
+from Controller import FileController, DataAssess
 from Lexicon.DepecheMood import DepecheMoodController
 from Lexicon.EmoSenticNet import EmoSenticNetController
 from Lexicon.NRC import NrcController
@@ -9,9 +9,7 @@ file_name = "dataset.csv"
 is_standard_model = True
 # LOAD DATASET
 df = pd.read_csv("03-post-nlp-"+file_name, sep=",")
-'''
 DataAssess.run(df)
-'''
 
 # RUN NRC SENTIMENT
 df = NrcController.run(df, is_standard_model)
