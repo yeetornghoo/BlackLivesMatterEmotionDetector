fbcc0be95a559408a497198b87767ddce7a5c2a0
yeetornghoo <carlson.hoo@gmail.com>
2020-09-18 10:37:04 +0800

    ISEAR

--- dataset/blm_minesota/3_analysis.py
+++ dataset/blm_minesota/3_analysis.py
@@ -1,8 +1,26 @@
 import pandas as pd
+from datetime import datetime
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from Controller import DataAssess
 
 df = pd.read_csv("04-post-sentiment-dataset.csv", sep=",")
 DataAssess.run(df)
 
+
+# SCORE AND COUNT COMPARISON
+
+
+# SCORE AND COUNT COMPARISON/ INTENSITY
+
+
+# SCORE AND COUNT COMPARISON / FIXED DATE / DATE
+
+
+# SCORE AND COUNT COMPARISON / FIXED DATE / HOUR
+
+
+# LINEAR V.S. LOG
 
--- dataset/blm_minesota_20200525_20200602/README.txt
+++
@@ -1,3 +0,0 @@
-SELECT * FROM getoldtweets3_tweet.final_blacklivesmatter_minnesota 
-WHERE tweet_created_dt > '2020-05-25 23:59:59' AND tweet_created_dt < '2020-06-02 00:00:00'
-order by retweets desc
