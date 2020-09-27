9abecc06c28e6cc7463ea3ebc65035806eadf9b8
yeetornghoo <carlson.hoo@gmail.com>
2020-09-27 12:18:55 +0800

    update

--- SecondDataset/MinneapolisPoliceIncident/0_process.py
+++ SecondDataset/MinneapolisPoliceIncident/0_process.py
@@ -48,9 +48,9 @@ df = pd.read_csv(source_dataset_file, sep=",")
 
 # GENERATE CHART TO COMPARE WITH MINESOTA DATA
 primary_df = get_blm_minesota(start_date, end_date)
-primary_df_group = primary_df[["tweet_created_date", "tweet_id"]].groupby(["tweet_created_date"], as_index=False).count()
+#primary_df_group = primary_df[["tweet_created_date", "nrc_sentiment", "tweet_id"]].groupby(["tweet_created_date", "nrc_sentiment"], as_index=False).count()
 
-
+'''
 def generate_plot(df_s, df_p, start_date, end_date, x_attr, y_attr, h_attr, y_factor, sub_report_name):
 
     # FIXED DATA ISSUE
@@ -82,7 +82,55 @@ def generate_plot(df_s, df_p, start_date, end_date, x_attr, y_attr, h_attr, y_fa
     plt.title("Total #BLM Tweets v.s. Minneapolis Police Incident ({})".format(sub_report_name.replace("_", " ")).upper(), fontsize='25')
     plt.savefig("{}img/lineplot_comparison_{}.png".format(dir_path, sub_report_name))
     plt.close()
+'''
 
 
-generate_plot(df, primary_df_group, start_date, end_date, "reportedDate", "caseNumber", "description", 30, "by_description")
-generate_plot(df, primary_df_group, start_date, end_date, "reportedDate", "caseNumber", "offense", 30, "by_offense")
+def generate_plot_by_mood(df_s, df_p, x_attr, y_attr, h_attr, sub_report_name, mood):
+
+    df_p = df_p.loc[df_p["nrc_sentiment"] == mood]
+    df_p_g = df_p[["tweet_created_date", "tweet_id"]].groupby(["tweet_created_date"], as_index=False).count()
+
+    # PLOT
+    fig, ax = plt.subplots(figsize=(25, 15))
+    # CREATE LINE PLOT FROM PRIMARY DATA
+    fig = sns.lineplot(y="tweet_id", x="tweet_created_date", data=df_p_g, ax=ax, linewidth=2.5, color='#000000')
+    fig.set(ylabel="Totol Tweet Count", xlabel="Date")
+    fig.set_xlabel(fig.get_xlabel(), fontsize=16)
+    fig.set_ylabel(fig.get_ylabel(), fontsize=16)
+
+    # CREATE SCATTER PLOT FROM SECONDARY DATA
+    fig = sns.lineplot(x=x_attr, y=y_attr, hue=h_attr, palette="muted", data=df_s)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor", fontsize='14')
+    plt.setp(ax.get_yticklabels(), rotation_mode="anchor",  fontsize='14')
+    plt.setp(fig.get_legend().get_texts(), fontsize='18')
+    plt.setp(fig.get_legend().get_title(), fontsize='22')
+
+    plt.title("Total #BLM Tweets v.s. Minneapolis Police Incident ({}, {})".format(sub_report_name.replace("_", " "), mood).upper(), fontsize='25')
+    plt.savefig("{}img/lineplot_comparison_{}_{}.png".format(dir_path, sub_report_name, mood))
+    plt.close()
+
+
+def generate_plot(df_s, df_p, start_date, end_date, x_attr, y_attr, h_attr, y_factor, sub_report_name):
+
+    # FIXED DATA ISSUE
+    df_s[x_attr] = df_s[x_attr].astype('datetime64[ns]')
+
+    # CREATE LINE PLOT FROM PRIMARY DATA
+    df_s = df_s.loc[(df_s[x_attr] >= start_date) & (df_s[x_attr] <= end_date)]
+    df_s[x_attr] = df_s[x_attr].apply(lambda x: datetime.strptime(str(x), DateHelper.standard_date_format).date())
+    df_group = df_s[[x_attr, y_attr, h_attr]].groupby([x_attr, h_attr], as_index=False).count()
+    df_group[y_attr] = df_group[y_attr]*y_factor
+
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "joy")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "sadness")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "fear")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "trust")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "disgust")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "surprise")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "anticipation")
+    generate_plot_by_mood(df_group, df_p, x_attr, y_attr, h_attr, sub_report_name, "anger")
+
+
+#generate_plot(df, primary_df, start_date, end_date, "reportedDate", "caseNumber", "description", 1, "by_description")
+generate_plot(df, primary_df, start_date, end_date, "reportedDate", "caseNumber", "offense", 1, "by_offense")
