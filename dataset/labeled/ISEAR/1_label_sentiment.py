fb2afc1836cc7b05ca055861f60ad2e75347bb5a
yeetornghoo <carlson.hoo@gmail.com>
2020-10-26 20:15:00 +0800

    a

--- dataset/labeled/ISEAR/1_label_sentiment.py
+++ dataset/labeled/ISEAR/1_label_sentiment.py
@@ -9,9 +9,10 @@ df = pd.read_csv("03-post-spelling-dataset.csv", sep=",")
 df = DataNLP.run(df)
 FileController.save_df_to_csv("04-post-nlp-dataset.csv", df)
 
-
+'''
 # CHECK SENTIMENT
 df = NrcController.run(df)
 df = DepecheMoodController.run(df)
 df = EmoSenticNetController.run(df)
 FileController.save_df_to_csv("05-post-sentiment-dataset.csv", df)
+'''
--- dataset/labeled/SemEval2019_Task3/1_label_sentiment.py
+++ dataset/labeled/SemEval2019_Task3/1_label_sentiment.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from Controller import FileController, DataAssess
+from Controller import FileController, DataAssess, DataNLP
 from Lexicon.DepecheMood import DepecheMoodController
 from Lexicon.EmoSenticNet import EmoSenticNetController
 from Lexicon.NRC import NrcController
@@ -11,9 +11,10 @@ df = pd.read_csv("03-post-spelling-dataset.csv", sep=",")
 df = DataNLP.run(df)
 FileController.save_df_to_csv("04-post-nlp-dataset.csv", df)
 
-
+'''
 # CHECK SENTIMENT
 df = NrcController.run(df)
 df = DepecheMoodController.run(df)
 df = EmoSenticNetController.run(df)
 FileController.save_df_to_csv("05-post-sentiment-dataset.csv", df)
+'''
