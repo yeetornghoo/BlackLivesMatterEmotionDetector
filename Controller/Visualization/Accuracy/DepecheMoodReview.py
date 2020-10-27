a89bff6baeed73a58f13f3c6aabfa564f9279a38
yeetornghoo <carlson.hoo@gmail.com>
2020-10-27 10:48:32 +0800

    isear

--- Controller/Visualization/Accuracy/DepecheMoodReview.py
+++ Controller/Visualization/Accuracy/DepecheMoodReview.py
@@ -9,22 +9,22 @@ from Controller import LogController
 def run(df, dir_path, dataset_name):
 
     LogController.log_h2("check dpm sentiment")
-    df_dpm = df[["text", "dpm_sentiment"]].groupby(["dpm_sentiment"], as_index=False).count()
+    df_dpm = df[["tweet_text", "dpm_sentiment"]].groupby(["dpm_sentiment"], as_index=False).count()
     print(df_dpm)
 
-    LogController.log_h2("check ori sentiment")
-    df_ori = df[["text", "ori_sentiment"]].groupby(["ori_sentiment"], as_index=False).count()
+    LogController.log_h2("check sentiment")
+    df_ori = df[["tweet_text", "sentiment"]].groupby(["sentiment"], as_index=False).count()
     print(df_ori)
 
-    df_tmp = df[["text", "ori_sentiment", "dpm_sentiment"]].groupby(["ori_sentiment", "dpm_sentiment"], as_index=False).count()
+    df_tmp = df[["tweet_text", "sentiment", "dpm_sentiment"]].groupby(["sentiment", "dpm_sentiment"], as_index=False).count()
 
     # CREATE PLIT
     save_file_name = "bar_{}_vs_dpm.png".format(dataset_name)
     sns.set_theme(style="whitegrid")
     f, ax = plt.subplots(figsize=(15, 6))
     sns.color_palette("tab10")
-    sns.barplot(x="ori_sentiment", y="text", data=df_ori, label="Total", color="y", alpha=0.2)
-    sns.barplot(x="ori_sentiment", y="text", hue="dpm_sentiment", data=df_tmp)
+    sns.barplot(x="sentiment", y="tweet_text", data=df_ori, label="Total", color="y", alpha=0.2)
+    sns.barplot(x="sentiment", y="tweet_text", hue="dpm_sentiment", data=df_tmp)
     plt.savefig("{}img/{}".format(dir_path, save_file_name))
     plt.close()
 
--- Controller/Visualization/Accuracy/NrcReview.py
+++ Controller/Visualization/Accuracy/NrcReview.py
@@ -9,22 +9,22 @@ from Controller import LogController
 def run(df, dir_path, dataset_name):
 
     LogController.log_h2("check nrc sentiment")
-    df_nrc = df[["text", "nrc_sentiment"]].groupby(["nrc_sentiment"], as_index=False).count()
+    df_nrc = df[["tweet_text", "nrc_sentiment"]].groupby(["nrc_sentiment"], as_index=False).count()
     print(df_nrc)
 
-    LogController.log_h2("check ori sentiment")
-    df_ori = df[["text", "ori_sentiment"]].groupby(["ori_sentiment"], as_index=False).count()
+    LogController.log_h2("check sentiment")
+    df_ori = df[["tweet_text", "sentiment"]].groupby(["sentiment"], as_index=False).count()
     print(df_ori)
 
-    df_tmp = df[["text", "ori_sentiment", "nrc_sentiment"]].groupby(["ori_sentiment", "nrc_sentiment"], as_index=False).count()
+    df_tmp = df[["tweet_text", "sentiment", "nrc_sentiment"]].groupby(["sentiment", "nrc_sentiment"], as_index=False).count()
 
     # CREATE PLIT
     save_file_name = "bar_{}_vs_nrc.png".format(dataset_name)
     sns.set_theme(style="whitegrid")
     f, ax = plt.subplots(figsize=(15, 6))
     sns.color_palette("tab10")
-    fig = sns.barplot(x="ori_sentiment", y="text", data=df_ori, label="Total", color="y", alpha=0.2)
-    sns.barplot(x="ori_sentiment", y="text", hue="nrc_sentiment", data=df_tmp)
+    fig = sns.barplot(x="sentiment", y="tweet_text", data=df_ori, label="Total", color="y", alpha=0.2)
+    sns.barplot(x="sentiment", y="tweet_text", hue="nrc_sentiment", data=df_tmp)
     plt.savefig("{}img/{}".format(dir_path, save_file_name))
     plt.close()
 
