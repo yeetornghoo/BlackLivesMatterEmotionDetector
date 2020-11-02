a9dddaf39903e01982b30bb293b4b1e4da992e6c
yeetornghoo <carlson.hoo@gmail.com>
2020-11-02 19:23:53 +0800

    a

--- dataset/labeled/smile_twitter/script_0_init.py
+++ dataset/labeled/smile_twitter/script_0_init.py
@@ -1,2 +1,3 @@
-#exec(open('script_1_process.py').read())
+exec(open('script_1_process.py').read())
 exec(open('script_2_generate_baseline.py').read())
+exec(open('script_3_quick_validation.py').read())
---
+++ dataset/labeled/smile_twitter/script_3_quick_validation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+from Controller.Validation import QuickValidation
+
+df = pd.read_csv("baseline-dataset.csv", sep=",")
+QuickValidation.run(df, "smile_twitter")
