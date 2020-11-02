0ea772e0d34ace48f59113b89dbab15e85c4b168
yeetornghoo <carlson.hoo@gmail.com>
2020-11-02 16:53:51 +0800

    Add Data Visualization Images

--- dataset/labeled/crownflower/script_1_process.py
+++ dataset/labeled/crownflower/script_1_process.py
@@ -8,22 +8,18 @@ df = pd.read_csv("dataset/text_emotion.csv", sep=",", names=["tweet_id", "sentim
 df['tweet_text'] = df['content']
 #DataAssess.run(df)
 
-
 # EXCLUDE NONE ENGLISH TEXT
 df = DataTranslation.run(df, "en")
 FileController.save_df_to_csv("01-post-translate-dataset.csv", df)
-
 
 # DATA CLEANING
 df = pd.read_csv("01-post-translate-dataset.csv", sep=",")
 df = DataCleaning.run(df)
 FileController.save_df_to_csv("02-post-cleaning-dataset.csv", df)
 
-
 # SPELLING CORRECTION
 df = pd.read_csv("02-post-cleaning-dataset.csv", sep=",")
 df = DataSpellingCorrection.run(df)
 FileController.save_df_to_csv("03-post-spelling-dataset.csv", df)
-
 
 LogController.log("Execution of 'script_1_process.py' is completed.")
