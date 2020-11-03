1a768e21230e07a4a692054daf90ed52c8d52869
yeetornghoo <carlson.hoo@gmail.com>
2020-11-03 18:04:09 +0800

    AUTO: UPDATE LATEST UNLABELED BASELINE DATASET

--- dataset/unlabeled/generate_labeled_baseline_dataset.py
+++ dataset/unlabeled/generate_labeled_baseline_dataset.py
@@ -1,33 +1,32 @@
-import os
 import pandas as pd
 import subprocess
-from Controller import DataAssess
-from Controller import FileController, DataCleaning, DataSpellingCorrection
+from Controller import FileController
 from Controller.Baseline import BaselineViz
 
 df = pd.DataFrame()
 dir_path = "C:/workspace/SocialMovementSentiment/dataset/unlabeled/"
 
-# blm_minnesota
+# GENERATE BASELINE DATASET
+
+# COMBINE FINAL BASELINE DATASET
 ds_minnesota_file = "{}/blm_minnesota/05-post-sentiment-dataset.csv".format(dir_path)
 ds_minnesota = pd.read_csv(ds_minnesota_file, sep=",")
 ds_minnesota = ds_minnesota[["tweet_text", "nrc_sentiment", "nrc_sentiment_score"]]
-
-# blm_washington
 ds_washington_file = "{}/blm_washington/04-post-sentiment-False-dataset.csv".format(dir_path)
 ds_washington = pd.read_csv(ds_washington_file, sep=",")
 ds_washington = ds_washington[["tweet_text", "nrc_sentiment", "nrc_sentiment_score"]]
-
-# COMBINE DATASETS
 df = pd.concat([ds_minnesota, ds_washington], axis=0)
 df.rename(columns={"nrc_sentiment": "sentiment", "nrc_sentiment_score": "sentiment_score"}, inplace=True)
 FileController.save_df_to_csv("{}master/baseline-dataset.csv".format(dir_path), df)
 
-df = pd.read_csv("{}master/baseline-dataset.csv".format(dir_path), sep=",")
-BaselineViz.run(df)
 
-'''
+# GENERATE VISUAL FOR THE LATEST BASELINE DATASET
+df = pd.read_csv("{}master/baseline-dataset.csv".format(dir_path), sep=",")
+out_path = "master/img/baseline/"
+BaselineViz.run_mood(df, out_path)
+
+
+# UPLOAD TO GITHUB
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "AUTO: UPDATE LATEST UNLABELED BASELINE DATASET"])
 subprocess.call(["git", "push"])
-'''
