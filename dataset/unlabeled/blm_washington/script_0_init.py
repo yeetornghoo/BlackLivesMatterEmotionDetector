9ace5e3c8134554654fe61f5c43f17688fc1dd1a
yeetornghoo <carlson.hoo@gmail.com>
2020-11-04 23:29:12 +0800

    upload

--- dataset/unlabeled/blm_washington/script_0_init.py
+++ dataset/unlabeled/blm_washington/script_0_init.py
@@ -1,3 +1,9 @@
+from Controller import GitController
+
 exec(open('script_1_process.py').read())
 exec(open('script_2_label_sentiment.py').read())
-exec(open('script_3_quick_validation.py').read())
+exec(open('script_3_analysis_lexicon.py').read())
+exec(open('script_4_generate_training_dataset.py').read())
+
+# COMMIT TO GIT
+GitController.commit("auto: update blm_washington")
