213173768bd8a0773710b811844d8cc85fa37cc1
Carlson <carlson.hoo@gmail.com>
2020-11-06 01:37:48 +0800

    a

--- dataset/unlabeled/blm_davideantonio/script_2_label_sentiment.py
+++ dataset/unlabeled/blm_davideantonio/script_2_label_sentiment.py
@@ -6,9 +6,9 @@ from Lexicon.NRC import NrcController
 
 # NLP TOKEN
 df = pd.read_csv("03-post-spelling-dataset.csv", sep=",")
-df.drop(['text'], axis=1, inplace=True)
 df = DataNLP.run(df)
 FileController.save_df_to_csv("04-post-nlp-dataset.csv", df)
+
 
 # CHECK SENTIMENT
 df = pd.read_csv("04-post-nlp-dataset.csv", sep=",")
@@ -17,6 +17,7 @@ df = NrcController.run(df)
 #df = EmoSenticNetController.run(df)
 
 # REMOVE COLUMNS
-df.drop(['place', 'retweet_count', 'favorite_count', 'lang', 'full_text'], axis=1, inplace=True)
+df.drop(['Unnamed: 0', 'place', 'retweet_count', 'favorite_count', 'lang', 'full_text'], axis=1, inplace=True)
 df.rename(columns={"nrc_sentiment": "sentiment", "nrc_sentiment_count": "sentiment_count", "nrc_sentiment_score": "sentiment_score"}, inplace=True)
 FileController.save_df_to_csv("05-post-sentiment-dataset.csv", df)
+
