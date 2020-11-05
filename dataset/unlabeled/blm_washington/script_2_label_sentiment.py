6030d35bf982a2915ef5a5041c189802ad8298c3
yeetornghoo <carlson.hoo@gmail.com>
2020-11-05 18:45:03 +0800

    washington dc

--- dataset/unlabeled/blm_washington/script_2_label_sentiment.py
+++ dataset/unlabeled/blm_washington/script_2_label_sentiment.py
@@ -10,10 +10,17 @@ df.drop(['text'], axis=1, inplace=True)
 df = DataNLP.run(df)
 FileController.save_df_to_csv("04-post-nlp-dataset.csv", df)
 
-
 # CHECK SENTIMENT
 df = pd.read_csv("04-post-nlp-dataset.csv", sep=",")
 df = NrcController.run(df)
 #df = DepecheMoodController.run(df)
 #df = EmoSenticNetController.run(df)
+
+# REMOVE COLUMNS
+df.drop(['tweet_id', 'permalink', 'username', 'to_person', 'mentions', 'hashtags', 'geo',
+         'record_inserted_date', 'state', 'radius', 'search_keyword', 'final_tweet_text'], axis=1, inplace=True)
+df.rename(columns={"nrc_sentiment": "sentiment",
+                   "nrc_sentiment_count": "sentiment_count",
+                   "nrc_sentiment_score": "sentiment_score"}, inplace=True)
+
 FileController.save_df_to_csv("05-post-sentiment-dataset.csv", df)
--- dataset/unlabeled/blm_washington/script_3_analysis_lexicon.py
+++ dataset/unlabeled/blm_washington/script_3_analysis_lexicon.py
@@ -9,21 +9,12 @@ from Helper import DateHelper
 # SETTING
 date_format = "%Y-%m-%d %H:%M:%S"
 min_intensity = 1.2
-dir_path = "C:/workspace/SocialMovementSentiment/dataset/blm_minnesota/"
+dir_path = "C:/workspace/SocialMovementSentiment/dataset/unlabeled/blm_washington/"
 focus_from_date = "2020-05-23"
 focus_to_date = "2020-06-05"
 
 # LOAD AND PREPARE DATASET
 df = pd.read_csv("05-post-sentiment-dataset.csv", sep=",")
-
-# REMOVE UNUSE COLUMN
-df.drop(['text', 'dpm_sentiment', 'dpm_sentiment_count', 'dpm_sentiment_score',
-         'esn_sentiment', 'esn_sentiment_count'], axis=1, inplace=True)
-
-df.rename(columns={"nrc_sentiment": "sentiment",
-                   "nrc_sentiment_count": "sentiment_count",
-                   "nrc_sentiment_score": "sentiment_score"}, inplace=True)
-
 
 # PREPARE THE ATTRIBUTE
 df['tweet_created_date'] = df['tweet_created_dt'].apply(lambda x: datetime.strptime(str(x), date_format).date())
