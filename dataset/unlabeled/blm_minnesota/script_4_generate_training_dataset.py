6f46fc71e10213eeaabb2ab78a407c1c14b4d6b6
Carlson <carlson.hoo@gmail.com>
2020-11-05 21:31:22 +0800

    minnesora

--- dataset/unlabeled/blm_minnesota/script_4_generate_training_dataset.py
+++ dataset/unlabeled/blm_minnesota/script_4_generate_training_dataset.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import seaborn as sns
 from Controller.Baseline import BaselineViz
-from Controller import GitController
+from Controller import GitController, FileController
+
+out_path = "img/baseline/"
 
 # SETTING
 sns.set_theme(style="whitegrid")
 df = pd.read_csv("05-post-sentiment-dataset.csv", sep=",")
 
-df = df.loc[:, ['tweet_id', 'nrc_sentiment', 'nrc_sentiment_score', "text"]]
-df.rename(columns={"nrc_sentiment": "sentiment",
-                   "nrc_sentiment_score": "sentiment_score",
-                   "text": "tweet_text"}, inplace=True)
-out_path = "img/baseline/"
+# FILTER WORD OF TWEET
+df['ttl_tweet_text_word'] = df['tweet_text'].str.split().str.len()
+df = df.loc[(df['ttl_tweet_text_word'] > 2)]
+
+# SAVE FILE
+df = df.loc[:, ['sentiment', 'sentiment_count', 'sentiment_score', "tweet_text"]]
+FileController.save_df_to_csv("baseline-dataset.csv", df)
 
 # GENERATE VISUAL FOR THE LATEST BASELINE DATASET
 BaselineViz.run_mood(df, out_path, 0.5)
 
 # COMMIT TO GIT
 # GitController.commit("auto: update latest unlabeled data - minnesota")
+
