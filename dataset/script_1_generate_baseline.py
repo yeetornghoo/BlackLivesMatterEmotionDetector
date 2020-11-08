e89fda00f83a0a3c1c7f74def5e71da52d849a88
CH LAB MACHINE 1 <carlson.hoo@gmail.com>
2020-11-08 21:10:41 +0800

    FIXED

--- dataset/script_1_generate_baseline.py
+++ dataset/script_1_generate_baseline.py
@@ -5,14 +5,13 @@ from Controller import FileController
 parent_path = "C:/workspace/SocialMovementSentiment/dataset/"
 
 # PROCESS LABELED DATA
-
 os.chdir("{}labeled".format(parent_path))
 exec(open('generate_baseline_dataset.py').read())
 
-# PROCESS LABELED DATA
+# PROCESS UNLABELED DATA
 os.chdir("{}unlabeled".format(parent_path))
 exec(open('generate_baseline_dataset.py').read())
 
-# PROCESS LABELED DATA
+# PROCESS MASTER DATA
 os.chdir("{}labeled".format(parent_path))
 exec(open('generate_baseline_dataset.py').read())
