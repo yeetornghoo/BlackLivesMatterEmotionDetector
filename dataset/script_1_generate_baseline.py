4e95b2d15b562eceaa2d3a178d49617b350a0643
CH LAB MACHINE 1 <carlson.hoo@gmail.com>
2020-11-08 10:02:13 +0800

    latest

--- dataset/script_1_generate_baseline.py
+++ dataset/script_1_generate_baseline.py
@@ -7,13 +7,13 @@ dir_path = "C:/workspace/SocialMovementSentiment/dataset/"
 
 
 # PROCESS LABELED DATA
-os.chdir("{}/labeled".format(dir_path))
+os.chdir("C:/workspace/SocialMovementSentiment/dataset/labeled")
 exec(open('generate_baseline_dataset.py').read())
 
 # PROCESS LABELED DATA
-os.chdir("{}/unlabeled".format(dir_path))
+os.chdir("C:/workspace/SocialMovementSentiment/dataset/unlabeled")
 exec(open('generate_baseline_dataset.py').read())
 
 # PROCESS LABELED DATA
-os.chdir("{}/master".format(dir_path))
+os.chdir("C:/workspace/SocialMovementSentiment/dataset/master")
 exec(open('script_1_create_and_validate_baseline_dataset.py').read())
